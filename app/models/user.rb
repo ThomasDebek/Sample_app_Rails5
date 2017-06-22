9f02647ba034ff7609673ddf8b00eb68365ad391
ThomasDebek <tom.debek@gmail.com>
2017-06-22 16:57:55 +0200

    rake db:migrate, oraz nowe pole zabezpieczające unikatowosc email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   validates :name,  presence: true, length: { maximum: 50 }
 
+
+  before_save { self.email = email.downcase }                 # To nam zapewni takze unikatowosc adresu a przy tym zapobiegnie skanowaniu calej bazy i znajdowaniu uzytkownikow po przez email 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
             format: { with: VALID_EMAIL_REGEX },
