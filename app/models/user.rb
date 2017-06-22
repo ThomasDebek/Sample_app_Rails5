1919e9b210278061858869e6b925917d1649e4ea
ThomasDebek <tom.debek@gmail.com>
2017-06-22 17:41:54 +0200

    Added validates and test for files email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
-  validates :name,  presence: true, length: { maximum: 50 }
 
 
-  before_save { self.email = email.downcase }                 # To nam zapewni takze unikatowosc adresu a przy tym zapobiegnie skanowaniu calej bazy i znajdowaniu uzytkownikow po przez email 
+
+  before_save { email.downcase! }
+  validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
             format: { with: VALID_EMAIL_REGEX },
