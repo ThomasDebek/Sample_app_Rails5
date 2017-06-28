34a22a91725c3144f31170480ff08b70ff45c68b
ThomasDebek <tom.debek@gmail.com>
2017-06-28 16:58:42 +0200

    Added flash messages to views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_url
+      redirect_to @user
+      flash[:success] = 'Welcome to the Sample App!'
     else
       render action: 'new'
     end
