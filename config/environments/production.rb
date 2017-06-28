aae126f4014153b4635df69a8ad977162f640898
ThomasDebek <tom.debek@gmail.com>
2017-06-28 17:14:37 +0200

    Zabezpiecznie danych zanim opuszczą naszą lokalną przeglądarke

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,9 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+
+  # ZABEZPIECZAMY SIE PO PRZEZ SZYFROWANIE ISTOTNYCH INFORMACJI ZANIM OPUSZCZA DANE NASZA LOKALNA PRZEGLADARKE
+   config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
