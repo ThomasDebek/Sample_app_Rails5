00955707fc06d22bc880b088745596f039f57232
ThomasDebek <tom.debek@gmail.com>
2017-06-28 15:36:43 +0200

    Added method crate to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,19 @@ class UsersController < ApplicationController
     #debugger
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_url
+    else
+      render action: 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
 end
