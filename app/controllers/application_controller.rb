e189399b7c4b2e1f77827c09f311ea9d0ffbfcd6
ThomasDebek <tom.debek@gmail.com>
2017-06-29 13:41:35 +0200

    Metoda przetrzymująca sessje uzytkownika

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include SessionsHelper  # W tej sekcji użytkownik zaloguje się przy użyciu tymczasowego pliku cookie sesji,
+                          # który wygasa automatycznie po zamknięciu przeglądarki
+
 
   def hello
     render html: "Hello Everyone"
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,13 @@
 module SessionsHelper
+
+ # dodanie tej metody - Spowoduje to umieszczenie tymczasowego pliku cookie w przeglądarce użytkownika
+ # zawierającego zaszyfrowaną wersję identyfikatora użytkownika,
+ # co umożliwia nam pobranie identyfikatora na kolejnych stronach
+ # przy użyciu sesji [: user_id].
+
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+
 end
